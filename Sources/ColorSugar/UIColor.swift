7d55cd9837bd2331b3a930a0dc299e42a5ac8c48
Ahmed Khalaf <pxlshpr@gmail.com>
2023-06-07 12:20:37 +0500

    tag increment

--- Sources/ColorSugar/UIColor.swift
+++ Sources/ColorSugar/UIColor.swift
@@ -1,6 +1,48 @@
 import UIKit
 
 public extension UIColor {
+    func darker(componentDelta: CGFloat = 0.1) -> UIColor {
+        return makeColor(componentDelta: -1*componentDelta)
+    }
+}
+
+extension UIColor {
+    private func makeColor(componentDelta: CGFloat) -> UIColor {
+        var red: CGFloat = 0
+        var blue: CGFloat = 0
+        var green: CGFloat = 0
+        var alpha: CGFloat = 0
+        
+        // Extract r,g,b,a components from the
+        // current UIColor
+        getRed(
+            &red,
+            green: &green,
+            blue: &blue,
+            alpha: &alpha
+        )
+        
+        // Create a new UIColor modifying each component
+        // by componentDelta, making the new UIColor either
+        // lighter or darker.
+        return UIColor(
+            red: add(componentDelta, toComponent: red),
+            green: add(componentDelta, toComponent: green),
+            blue: add(componentDelta, toComponent: blue),
+            alpha: alpha
+        )
+    }
+}
+
+extension UIColor {
+    // Add value to component ensuring the result is
+    // between 0 and 1
+    private func add(_ value: CGFloat, toComponent: CGFloat) -> CGFloat {
+        return max(0, min(1, toComponent + value))
+    }
+}
+
+public extension UIColor {
     convenience init(hex: String) {
         let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
         var int: UInt64 = 0
