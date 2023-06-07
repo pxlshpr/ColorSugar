838cc13f3153793982dd315ed94bf3c4941c9566
Ahmed Khalaf <pxlshpr@gmail.com>
2023-06-07 11:02:07 +0500

    tag increment

--- Sources/ColorSugar/CGColor.swift
+++ Sources/ColorSugar/CGColor.swift
@@ -1,23 +1,7 @@
 import Foundation
 import CoreGraphics
 
-//MARK: Hex
 public extension CGColor {
-    
-    //TODO: Consolidate having two of these
-    var hex: String {
-        let r: CGFloat = components?[0] ?? 0.0
-        let g: CGFloat = components?[1] ?? 0.0
-        let b: CGFloat = components?[2] ?? 0.0
-
-        let hexString = String.init(
-            format: "%02lX%02lX%02lX",
-            lroundf(Float(r * 255)),
-            lroundf(Float(g * 255)),
-            lroundf(Float(b * 255))
-        )
-        return hexString.lowercased()
-    }
     
     var hexString: String {
         let components = components
@@ -25,7 +9,8 @@ public extension CGColor {
         let g: CGFloat = components?[1] ?? 0.0
         let b: CGFloat = components?[2] ?? 0.0
 
-        let hexString = String.init(format: "%02lX%02lX%02lX", lroundf(Float(r * 255)), lroundf(Float(g * 255)), lroundf(Float(b * 255)))
+        let hexString = String.init(
+            format: "%02lX%02lX%02lX", lroundf(Float(r * 255)), lroundf(Float(g * 255)), lroundf(Float(b * 255)))
         return hexString
     }
 }
